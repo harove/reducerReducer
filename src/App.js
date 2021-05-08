849d6a16cc06075c83985a740a1d1d5e50f2ee1f
Edgar Haro <harove@gmail.com>
2021-05-08 19:01:56 -0400

    testing reducer callback

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ export default function App() {
   const nums = [2, 4, 6];
   const initialState = 0;
 
-  function reducer(state, value) {
+  function reducer(state, value, currentIndex, arr) {
+    console.log(currentIndex);
+    console.log(arr);
     return state + value;
   }
 
   let sum = nums.reduce(reducer, initialState);
 
   console.log(sum);
+  console.log(reducer(2, 1));
 
   return (
     <div className="App">
